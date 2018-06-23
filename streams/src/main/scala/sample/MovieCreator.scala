b5b0f6adc6e83997a625af4921e459c8519a06c8
vitaliihonta <murcielago13ua@gmail.com>
2018-06-23 16:00:11 +0300

    movie creator impl

--- streams/src/main/scala/sample/MovieCreator.scala
+++ streams/src/main/scala/sample/MovieCreator.scala
@@ -4,9 +4,13 @@ import akka.actor._
 import akka.stream._
 import scala.concurrent.{ExecutionContext, Future}
 import java.nio.file._
+import akka.NotUsed
 import com.typesafe.scalalogging.StrictLogging
-import io.circe.syntax._
 import model._
+import akka.stream.scaladsl._
+import akka.util.ByteString
+import io.circe.syntax._
+import io.circe.parser._
 
 object MovieCreator extends StrictLogging {
   def main(args: Array[String]): Unit = {
@@ -15,6 +19,38 @@ object MovieCreator extends StrictLogging {
     implicit val ec: ExecutionContext = ExecutionContext.global
 
     val filePath = Paths.get("./movies.txt")
+
+    val fileSource: Source[ByteString, Future[IOResult]] =
+      FileIO.fromPath(filePath)
+
+    val linesFlow = Flow[ByteString]
+      .via(Framing.delimiter(ByteString("\n"), 1000))
+      .map(_.utf8String)
+
+    val moviesFlow = Flow[String]
+      .map(decode[model.Movie])
+      .mapConcat {
+        case Left(error) =>
+          logger.warn(s"Error during parsing: $error")
+          Nil
+
+        case Right(movie) =>
+          List(movie)
+      }
+
+    val printSink =
+      Sink.foreach[model.Movie](movie => logger.info(s"Parsed movie: $movie"))
+
+    val graph = fileSource
+      .via(linesFlow)
+      .via(moviesFlow)
+      .toMat(printSink)(Keep.right)
+
+    val futureDone = graph.run()
+    futureDone.onComplete { done =>
+      logger.info(done.toString)
+      system.terminate()
+    }
   }
 }
 
